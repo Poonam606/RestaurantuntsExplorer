5cc37eddee039daa7952a8c551976b10077e08d7
Poonam606 <poonamyadav464@gmail.com>
2025-06-09 15:00:15 +0530

    Initial Commit

---
+++ RestaurantuntsExplorer/RestaurantuntsExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantuntsExplorerApp.swift
+//  RestaurantuntsExplorer
+//
+//  Created by poonam on 09/06/25.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantuntsExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
